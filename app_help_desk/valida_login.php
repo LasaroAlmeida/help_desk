c90d514c412cf3502ff167ba5bed5bc1e6639766
Lazlo-coder <lasarodalmeida@gmail.com>
2022-01-23 11:02:48 -0300

    usando sessoes

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+$usuario_autenticado =  false;
+$usuarios_app = array(
+    array("email" => "lasarodalmeida@gmail.com", "senha" => '123'),
+    array("email" => "adm@teste.com", "senha" => '12345'),
+    array("email" => "pedro@teste.com", "senha" => 'pedroo'),
+    array("email" => "doc@zica.com", "senha" => '2424'),
+);
+// echo '<pre>';
+// print_r($usuarios_app);
+// echo '<pre>';
+foreach ($usuarios_app as $user) {
+    if ($user["email"] == $_POST["email"] && $user["senha"] == $_POST["senha"]) {
+        $usuario_autenticado = true;
+    }
+}
+
+if ($usuario_autenticado) {
+    echo "Usuário autenticado";
+    $_SESSION["autenticado"] = "SIM";
+} else {
+    $_SESSION["autenticado"] = "NAO";
+    header("Location:index.php?login=erro");
+}
